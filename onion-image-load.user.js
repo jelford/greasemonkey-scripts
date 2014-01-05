d77141623cce6b5b7db84112907f5a85f3a97081
James Elford <james.p.elford@gmail.com>
2014-01-05 12:48:23 +0000

    Added loading of the retina-version of the image by default

--- onion-image-load.user.js
+++ onion-image-load.user.js
@@ -4,11 +4,12 @@
 // @description Surrogate script to properly load images from The Onion
 // @match       http://*.theonion.com/*
 // @include     http://*.theonion.com/*
-// @version     2
+// @version     3
 // @grant       none
 // @downloadURL https://github.com/jelford/greasemonkey-scripts/raw/master/onion-image-load.user.js
 // ==/UserScript==
 
 Array.forEach(Array.filter(document.getElementsByTagName('img'), function(img) { return img.classList.contains('lazy-load') }), function(img) {
-    img.src = img.dataset.src;
+    img.src = img.dataset.retinaSrc ? img.dataset.retinaSrc : img.dataset.src;
 });
+
