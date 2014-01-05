62422d17fd3116ad721c0aa8ea9a6e237d2141a6
James Elford <james.p.elford@gmail.com>
2014-01-05 12:25:16 +0000

    added image loading functionality

--- onion-image-load.user.js
+++ onion-image-load.user.js
@@ -1,11 +1,13 @@
 // ==UserScript==
-// // // @name        Load Onion Images
-// // // @namespace   com.jameselford.scripts
-// // // @description Surrogate script to properly load images from The Onion
-// // // @include     http://www.theonion.com/*
-// // // @version     1
-// // // @grant       none
-// // // @downloadURL 
-// // // ==/UserScript==
+// @name        Load Onion Images
+// @namespace   com.jameselford.scripts
+// @description Surrogate script to properly load images from The Onion
+// @match       http://*.theonion.com/*
+// @include     http://*.theonion.com/*
+// @version     1
+// @grant       none
+// ==/UserScript==
 
-alert('hi')
+Array.forEach(Array.filter(document.getElementsByTagName('img'), function(img) { return img.classList.contains('lazy-load') }), function(img) {
+    img.src = img.dataset.src;
+});
