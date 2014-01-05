cc3666f7b9adc4f039dd0ebf7ac83561c3cc4d75
James Elford <james.p.elford@gmail.com>
2014-01-05 12:46:49 +0000

    Added download uri

--- onion-image-load.user.js
+++ onion-image-load.user.js
@@ -4,8 +4,9 @@
 // @description Surrogate script to properly load images from The Onion
 // @match       http://*.theonion.com/*
 // @include     http://*.theonion.com/*
-// @version     1
+// @version     2
 // @grant       none
+// @downloadURL https://github.com/jelford/greasemonkey-scripts/raw/master/onion-image-load.user.js
 // ==/UserScript==
 
 Array.forEach(Array.filter(document.getElementsByTagName('img'), function(img) { return img.classList.contains('lazy-load') }), function(img) {
